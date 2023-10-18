212344077421ee2f0c8dd5cad8b130aede04d799
chirag <chiragverma2207@gmail.com>
2023-10-18 23:35:10 +0530

    test

---
+++ public/assets/scripts/core/bank_ctrl.js
@@ -0,0 +1,20 @@
+app.controller('bankCtrl', function($scope , $http, $timeout , DBService,Upload) {
+    
+    $scope.bank_name = "";
+    $scope.purpose_loading = false;
+
+    $scope.addBank = function(){
+        $("#bankModal").modal("show"); 
+    }
+
+    $scope.submitBank = function(){
+        $scope.purpose_loading = true;
+        DBService.postCall({bank_name : $scope.bank_name}, '/api/banks/store').then((data) => {
+            alert(data.message);
+            $scope.bank_name = "";
+             $scope.purpose_loading = false;
+             $("#bankModal").modal("hide"); 
+        });
+    }
+
+});
